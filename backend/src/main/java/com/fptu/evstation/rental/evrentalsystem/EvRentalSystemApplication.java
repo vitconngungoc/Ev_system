c0c978084e4310a56b3f202f4a283c351e47cc60
Nguyễn Quang Tùng <164199363+vitconngungoc@users.noreply.github.com>
2025-10-28 00:05:00 +0700

    Initial commit

---
+++ backend/src/main/java/com/fptu/evstation/rental/evrentalsystem/EvRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fptu.evstation.rental.evrentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvRentalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvRentalSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/fptu/evstation/rental/evrentalsystem/EvRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fptu.evstation.rental.evrentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvRentalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
