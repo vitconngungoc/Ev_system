5d7f03db14a042b89e78988a18a76b04f5a9fab4
TriDung-fpt <dungnnt0910@gmail.com>
2025-11-04 11:37:29 +0700

    feature: Implement station management service

---
+++ backend/src/main/java/com/fptu/evstation/rental/evrentalsystem/repository/StationRepository.java
@@ -0,0 +1,7 @@
+package com.fptu.evstation.rental.evrentalsystem.repository;
+
+import com.fptu.evstation.rental.evrentalsystem.entity.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StationRepository extends JpaRepository<Station,Long> {
+}
